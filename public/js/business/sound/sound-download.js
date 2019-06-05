363f92e0ce46cccd9a5794430e5fe6a5d5683a36
mazl201 <mazl32@163.com>
2019-06-05 22:39:18 +0800

    baidu voice

--- public/js/business/sound/sound-download.js
+++ public/js/business/sound/sound-download.js
@@ -1,15 +1,42 @@
 debugger;
+var startTime;
+var totalTime;
+var currentTime;
+var nowAudio;
+var nowInterval;
+
+function freshAudioContent(){
+    console.log( "当前播放进度(实时)"+nowAudio.currentTime);
+    console.log( "总长度（实时）"+nowAudio.duration);
+    console.log("总时间"+totalTime);
+    console.log("当前进度"+currentTime);
+    console.log("开始时间"+startTime);
+    console.log("现在时间"+new Date());
+    var content = $($(nowAudio).parent().find(".content")[0]).html();
+    var contentLength = content.length;
+    var calucIndex = parseInt((nowAudio.currentTime/nowAudio.duration)*contentLength);
+    var calucEnd = (calucIndex + 20)
+    if(calucIndex < 0){
+        calucIndex = 0;
+    }
+    if(calucEnd > contentLength){
+        calucEnd = contentLength;
+    }
+    debugger;
+    $($(nowAudio).parent().find(".contentDis")[0]).html(content.substring(calucIndex,calucEnd));
+}
+
 
 $(function(){
     debugger;
     var audios = $("audio");
     for(var i in audios){
         if($(audios[i])){
-            $(audios[i]).on("canplay",function(){
-                var audioName = $(this).attr("name");
-                console.log(audioName+"可以开始播放")
-                $(this).show();
-            })
+            // $(audios[i]).on("canplay",function(){
+            //     var audioName = $(this).attr("name");
+            //     console.log(audioName+"可以开始播放")
+            //     $(this).show();
+            // })
             $(audios[i]).on("play",function(){
                 var audioName = $(this).attr("name");
                 console.log(audioName+"开始播放")
@@ -19,8 +46,13 @@ $(function(){
                 console.log(audioName + "百分比"+ number*100)
                 var contentLength = $($(this).parent().find(".content")[0]).html().length;
                 console.log(audioName + "目前字数"+parseInt(contentLength*number))
-
+                currentTime = this.currentTime;
+                totalTime = this.duration;
+                startTime = new Date();
+                nowAudio =this;
+                nowInterval = setInterval(freshAudioContent,1000);
             })
+
             $(audios[i]).on("pause",function(){
                 var audioName = $(this).attr("name");
                 console.log(audioName+"暂停播放")
@@ -32,6 +64,7 @@ $(function(){
                 console.log(audioName + "目前字数"+parseInt(contentLength*number))
                 var startIndex = 0;
                 var endIndex = 30;
+                clearInterval(nowInterval);
                 if(startIndex < 0){
                     startIndex = 0;
                 }
