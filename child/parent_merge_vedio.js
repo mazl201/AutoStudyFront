c79e8c7b232ba84a0cb49fe2744162e16ffd6d5d
mazl201 <mazl2010@qq.com>
2019-08-01 17:20:37 +0800

    百度识别，图像中 文字

--- child/parent_merge_vedio.js
+++ child/parent_merge_vedio.js
@@ -10,33 +10,35 @@ var childs = [];
 for (var i = 0; i < numCPUs; i++) {
     childs[i] = child_process.fork("./child/child_merge_vedio.js", [i]);
 
-    childs[i].on('message', function(m) {
-        if(m.indexOf("leisure-") > -1 && !working){
+    childs[i].on('message', function (m) {
+        if (m.indexOf("leisure-") > -1 && !working) {
             working = true;
-            let cid = m.replace("leisure-","");
-            console.log("第"+ cid+"个子进程，空闲中.");
-            if(readyToResolve && readyToResolve.length && readyToResolve.length > 0){
+            let cid = m.replace("leisure-", "");
+            console.log("第" + cid + "个子进程，空闲中.");
+            if (readyToResolve && readyToResolve.length && readyToResolve.length > 0) {
                 var readyToResolveElement = readyToResolve[0];
                 let resolvingName = (new Date()).getTime() + cid;
+                readyToResolve.splice(0, 1);
                 alreadyToResolving[resolvingName] = readyToResolveElement;
                 //删除原有的 元素
-                childs[cid].send({hasWork:"yes",workContent:readyToResolveElement});
-
-
-               working = false;
-            }else{
-                childs[cid].send({hasWork:"no"});
+                childs[cid].send({hasWork: "yes", workContent: readyToResolveElement});
+                working = false;
+            } else {
+                childs[cid].send({hasWork: "no"});
                 working = false;
             }
-
+        }
+        if (m.indexOf("complete-") > -1) {
+            var resolvedName = m.replace("complete-", "");
+            alreadyToResolving[resolvedName] = null;
         }
         console.log('父进程正在接受子进程消息', m);
     });
 
-    childs[i].send({hasWork:"父进程发送给第"+i+"个进程."})
+    childs[i].send({hasWork: "父进程发送给第" + i + "个进程."})
 
     childs[i].on('close', function (code) {
-        console.log('子进程'+i+'已退出，退出码 ' + code);
+        console.log('子进程' + i + '已退出，退出码 ' + code);
     });
 }
 
